83aa26ab583dc1ff5c8b0298db04564c457dc301
Matias Paz <neoxap@gmail.com>
2023-10-30 13:16:47 -0300

    Requerimiento Desafío 1

--- js/javascript.js
+++ js/javascript.js
@@ -2,8 +2,12 @@ var valorEnDolar = 155.5;
 
 var cotizacionEnPesos = 16.893;
 
+var cotizacionEnBitcoin = 34585.20;
+
 var valorEnPesos = valorEnDolar * cotizacionEnPesos;
+var valorEnCripto = valorEnDolar * cotizacionEnBitcoin; 
 
 valorEnPesos = valorEnPesos.toFixed(2);
+valorEnCripto = valorEnCripto.toFixed(2);
 
-alert ("$"+valorEnPesos);
+alert ("$"+valorEnPesos+"\n"+"$BTC"+valorEnCripto);
