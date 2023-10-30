ae79c85edece10bf3ed74f62d205b980d55b8359
Matias Paz <neoxap@gmail.com>
2023-10-30 13:45:19 -0300

    Requerimiento Desafío 2 y 3

--- js/javascript.js
+++ js/javascript.js
@@ -1,3 +1,5 @@
+var nombreVisitante = "Carlitos"
+
 var valorEnDolar = 155.5;
 
 var cotizacionEnPesos = 16.893;
@@ -10,4 +12,12 @@ var valorEnCripto = valorEnDolar * cotizacionEnBitcoin;
 valorEnPesos = valorEnPesos.toFixed(2);
 valorEnCripto = valorEnCripto.toFixed(2);
 
-alert ("$"+valorEnPesos+"\n"+"$BTC"+valorEnCripto);
+var valorCentigrados = 24;
+
+var valorFarenheit = (valorCentigrados * 9/5) + 32;
+
+var valorKelvin = valorCentigrados + 273.15;
+
+alert ("Hola, "+nombreVisitante+"!"+"\n"+"Presiona el botón para ver las cotizaciones del Dólar Estadounidense a Pesos Mexicanos y en Bitcoin")
+alert ("$Mex "+valorEnPesos+"\n"+"$BTC "+valorEnCripto);
+alert ("Solo para que lo sepas, la temperatura actual es de:"+"\n"+"ºC "+valorCentigrados+"\n"+"ºF "+valorFarenheit+"\n"+"ºK "+valorKelvin)
